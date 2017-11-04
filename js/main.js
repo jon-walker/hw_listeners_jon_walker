11b3551f114f618d37d7c810c6675781cc0b890f
Jonathan Walker <jonathan.walker@ufl.edu>
2017-11-04 14:08:15 -0400

    Map out logic of needed scripts in pseudocode

---
+++ js/main.js
@@ -0,0 +1,16 @@
+// FORM HANDLING FUNCTION
+
+    // Prevent Default
+
+    // Gather form data
+
+    // Create log messages
+
+    // Log messages to the console.
+
+
+// SUBMIT BUTTON LISTENER
+
+// Get submit button through id.
+
+// Listen for click on submit button
