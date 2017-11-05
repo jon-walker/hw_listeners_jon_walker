b598653e95f8ff5be1939a88f14c2cd263dbca8a
Jonathan Walker <jonathan.walker@ufl.edu>
2017-11-04 20:47:16 -0400

    Create menu dropdown function triggered by click

--- js/main.js
+++ js/main.js
@@ -2,21 +2,20 @@
 
 function dropdownMenu() {
     // Check for class of activenav.
-    var statusCheck = document.querySelector('ul.activenav');
+    var navState = document.querySelector('ul.activenav');
 
     // If menu does not have class of activenav.
-    if (statusCheck = null) {
+    if (navState == null) {
 
+        // Add class of activenav to menu.
+        var inactiveNav = document.querySelector('nav ul');
+        inactiveNav.setAttribute('class', 'activenav');
+    } else {
+        navState.removeAttribute('class', 'activenav');
     }
-    // Add class of activenav to menu.
-    // Remove class of hide.
-
-    // If menu has class of activenav.
-    else {
-
-    }
-    // Add class of hide to menu.
     // Remove class of activenav.
 }
 
 // Create listener for click on menu button
+var menuButton = document.getElementById('dropdown');
+menuButton.addEventListener('click', dropdownMenu);
