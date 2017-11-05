6ad55a63060d8b761389def602e8b862a368d72f
Jonathan Walker <jonathan.walker@ufl.edu>
2017-11-05 07:41:01 -0500

    Clarify roles of event listeners and functions in the comments

--- js/main.js
+++ js/main.js
@@ -15,7 +15,8 @@ function dropdownMenu() {
     }
 }
 
-// Create listener for click on menu button
+// Creates event listener for click on Menu button
+// On click, the dropdownMenu function fires which toggles class names on nav ul which results in displaying or hiding the navigation links depending on the state of the ul at the time the menu is clicked.
 var menuButton = document.getElementById('dropdown');
 menuButton.addEventListener('click', dropdownMenu);
 
@@ -28,7 +29,10 @@ function colorChange() {
     // Add a class
 }
 
-// Create listener for hover over paragraph
+// ***** EVENT LISTENER #2 *******
+// Creates event listener that is triggered by cursor moving over top the first (and only) paragraph with the hoverChange class.
+// When cursor enters the space, a class is added that changes background-color
+// Note: I chose not to use ID because specificity would be an issue in the CSS.
 var hoverTarget = document.getElementsByClassName('hoverChange');
 hoverTarget[0].addEventListener('mouseenter', colorChange);
 
@@ -41,5 +45,7 @@ function colorRevert() {
     // Add a class
 }
 
-// Create listener for hover over paragraph
+// ***** EVENT LISTENER #3 *******
+// Creates event listener that is triggered by cursor moving off of the first (and only) paragraph with the hoverChange class.
+// When cursor leaves the space, a class added previously is removed that changes background-color back to orginal color
 hoverTarget[0].addEventListener('mouseleave', colorRevert);
