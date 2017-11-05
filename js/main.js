443c145c3e30ee13b5fab2db150fcc70de74e986
Jonathan Walker <jonathan.walker@ufl.edu>
2017-11-04 20:44:58 -0400

    Create conditional based on state of nav menu

---
+++ js/main.js
@@ -0,0 +1,22 @@
+// DROPDOWN MENU FUNCTION
+
+function dropdownMenu() {
+    // Check for class of activenav.
+    var statusCheck = document.querySelector('ul.activenav');
+
+    // If menu does not have class of activenav.
+    if (statusCheck = null) {
+
+    }
+    // Add class of activenav to menu.
+    // Remove class of hide.
+
+    // If menu has class of activenav.
+    else {
+
+    }
+    // Add class of hide to menu.
+    // Remove class of activenav.
+}
+
+// Create listener for click on menu button
